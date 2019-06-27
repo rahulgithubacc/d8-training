0ef7220f2047c8b16b5ed077ff820ca9227b30e2
rahulqed <rahul.deshmukh324.rdr@gmail.com>
2019-06-27 18:32:10 +0530

    Updated code

--- web/modules/custom/clockmodule/src/Services/Myservice.php
+++ web/modules/custom/clockmodule/src/Services/Myservice.php
@@ -4,7 +4,7 @@ class MyService{
     public function myservice()
     {
 			$client=new \GuzzleHttp\Client();
-			$response=$client->request('http://worldclockapi.com/api/json/est/now');
+			$response=$client->request('GET', 'http://worldclockapi.com/api/json/est/now?q='.'&timeZoneName=Eastern Standard Time');
 			// print_r($response);
 			return $response->getBody();
     }
